9f3c51d3a07484805fd391691133715ffd1fd995
professornaite <professornaite@gmail.com>
2025-02-06 14:43:14 -0500

    cre tract data

---
+++ code/Untitled.py
@@ -0,0 +1,20 @@
+%load_ext autotime
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import requests
+from census import Census
+from us import states
+
+import requets
+import json
+
+url = 
+
+response = request.get(url)
+variables = response.json()
+
+variables_ids= [
+  'B01001_001E', 'B01001B_001E', 'BO1001H_001E'
+]
